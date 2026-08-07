298480335ce152d923befa4ac6f99b3e394b944b
chaitali-more <chaitali1266@gmail.com>
2026-08-07 11:44:49 +0530

    fix: default api fallback to deployed url and correct signup status code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,7 +7,7 @@ const signupUser = async (req, res) => {
         const { name, email, password } = req.body;
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(404).json({
+            return res.status(400).json({
                 message: "User already exists",
             })
         }
